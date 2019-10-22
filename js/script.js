407e3b1c2aaed3f7df5bfa07fd1a1ef093baf894
bss94 <serjak.2012@gmail.com>
2019-10-22 14:40:20 +0600

    update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$(".project-carousel").owlCarousel({
+    loop: true,
+    items:2,
+    nav: true,
+    responsive: {
+        600: {
+            items: 1
+        },
+        900:{
+            items: 2
+        }}
+
+
+
+});
+
+
+});
