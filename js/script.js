4bac49e5ddd8091bc22368d496a1ad3cc5757c3a
bss94 <serjak.2012@gmail.com>
2019-11-06 22:48:33 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
 $(".project-carousel").owlCarousel({
     loop: true,
-    items:2,
     nav: true,
+    items:2,
+    dots:false,
     responsive: {
+
+        320: {
+            items: 1
+        },
         600: {
             items: 1
         },
         900:{
             items: 2
         }}
+});
+    $(".rent-car").owlCarousel({
+        loop: true,
+        nav: true,
+        items:1,
+        dots:false,
 
-
+    }   );
 
 });
-
-
+$('.burger-menu').on('click', function (e) {
+    e.preventDefault();
+    $('.burger-menu').toggleClass('burger-open');
+    $('.menu').toggleClass('menu-show');
 });
