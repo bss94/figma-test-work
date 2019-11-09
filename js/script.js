48e5ad6972f65911548807417bb3631f6c33dc8f
bss94 <serjak.2012@gmail.com>
2019-11-09 09:28:25 +0600

    update

--- js/script.js
+++ js/script.js
@@ -29,6 +29,7 @@ $('.burger-menu').on('click', function (e) {
     e.preventDefault();
     $('.burger-menu').toggleClass('burger-open');
     $('.menu').toggleClass('menu-show');
+    $('.animation-box').toggleClass('menu-anim');
 });
 
 $(document).ready(function() {
